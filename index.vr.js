a5082b18193e36aeb7adca5df1ff891bfd3f75e0
Oh, Isaac (US - Los Angeles) <ioh@deloitte.com>
2017-08-18 10:35:18 -0700

    Changes

--- index.vr.js
+++ index.vr.js
@@ -7,12 +7,53 @@ import {
   Text,
   View,
 } from 'react-vr';
+import Canvas from './components/Canvas';
+import UI from './components/UI';
+
+const Config = [
+  {
+    key: 0,
+    imageSrc: 'inside-bar.jpg',
+    buttonImageSrc: 'inside-bar.jpg',
+  },
+  {
+    key: 1,
+    imageSrc: 'inside-sofas.jpg',
+    buttonImageSrc: 'inside-sofas.jpg',
+  },
+  {
+    key: 2,
+    imageSrc: 'outside-gate.jpg',
+    buttonImageSrc: 'outside-gate.jpg',
+  },
+  {
+    key: 3,
+    imageSrc: 'outside-benches.jpg',
+    buttonImageSrc: 'outside-benches.jpg',
+  }
+];
 
 export default class GDVR_REACTVR_SITEPOINT_GALLERY extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      src: 'chess-world.jpg',
+    };
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Canvas
+          src={this.state.src}
+        />
+        <UI
+          buttonConfig={Config}
+          onClick={(key)=>{
+            this.setState({src: Config[key].imageSrc});
+          }}
+        />
         <Text
           style={{
             backgroundColor: '#777879',
@@ -23,9 +64,9 @@ export default class GDVR_REACTVR_SITEPOINT_GALLERY extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 1, -3]}],
           }}>
-          hello
+          Find Us!
         </Text>
       </View>
     );
